01c4a46212d7373384e15bae9a819ed65f2e1cb0
Muhammad Iqbal <alfarizky111122@gmail.com>
2026-04-11 11:20:06 +0700

    Add files via upload

---
+++ PemrogramanWebP6/Latihan1.php
@@ -0,0 +1,11 @@
+<?php
+$personal = array(
+    "nama" => "Salman",
+    "kendaraan" => "Minibus",
+    "rumah" => "Perumnas"
+);
+
+print $personal["nama"] . "\n";
+print $personal["kendaraan"] . "\n";
+print $personal["rumah"] . "\n";
+?>
---
+++ PemrogramanWebP6/Latihan2.php
@@ -0,0 +1,9 @@
+<?php
+$personal["nama"] = "Salman";
+$personal["kendaraan"] = "Minibus";
+$personal["rumah"] = "Perumnas";
+
+print $personal["nama"] . "\n";
+print $personal["kendaraan"] . "\n";
+print $personal["rumah"] . "\n";
+?>
